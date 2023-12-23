8cd91b16566b5e5912a6a17d728125464f7d8c9b
MoumitaRashidBushra <moumitarashidbushra@gmail.com>
2023-12-23 13:41:49 +0600

    first commit

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function calculateBMI(event) {
     var calories = calculateCalories(age, gender, weight, height, activityLevel);
 
     // Display results
-    var resultText = "Your BMI is: " + bmi + " kg/m2\n\n";
+    var resultText = name + " BMI is: " + bmi + " kg/m2\n\n";
     resultText += "BMI Range: " + bmiRange + "\n\n";
     resultText += "Your calories are: " + calories.toFixed(0) + " kcal/day \n\n";
-    resultText += "Healthy BMI range: 18.5 kg/m2 - 25 kg/m2";
+
 
     document.getElementById("result").innerText = resultText;
 
@@ -35,8 +35,8 @@ function calculateBMI(event) {
                 <h5 class="card-title text-center mb-2">Are you Satisfied with your BMI?</h5>
                
                 <div class="d-flex justify-content-around align-items-center mt-2 mb-3">
-                    <div><a href="index.html" class="btn custom-button-bg text-white">Yes (Back to Home)</a></div>
-                    <div><a href="dietFood.html" class="btn custom-button-bg text-white">Food Recommendation</a></div>
+                    <div><a href="index.html" class="btn custom-button-bg text-white py-3">Yes (Back to Home)</a></div>
+                    <div><a href="dietFood.html" class="btn custom-button-bg text-white">Custom food recommendation <br> <span class="small-font">based on you expected BMI</span> </a></div>
                 </div>
             </div>
         </div>
@@ -46,6 +46,19 @@ function calculateBMI(event) {
     var cardContainer = document.getElementById("cardContainer");
     cardContainer.innerHTML = ''; // Clear previous results
     cardContainer.appendChild(cardElement);
+
+
+
+    // Get values from the form
+
+
+
+    // Fetch the JSON data (assuming your JSON file is named 'diet_data.json')
+    fetch('/diet/dietFoods.json')
+        .then(response => response.json())
+        .then(data => displayDietRecommendations(data, bmi, age, gender))
+        .catch(error => console.error('Error fetching data:', error));
+
 }
 
 function getBmiRange(bmi) {
@@ -83,4 +96,77 @@ function getActivityMultiplier(activityLevel) {
         default:
             return 1.2;
     }
+}
+
+
+
+function displayDietRecommendations(data, bmi, age, gender) {
+    // Filter data based on user input
+    const userDietData = data.filter(item =>
+        bmi >= item.bmiValue.min && bmi <= item.bmiValue.max &&
+        age >= item.age.min && age <= item.age.max &&
+        gender === item.gender
+    );
+
+    // Display diet recommendations in the specified HTML format
+    const dietFoodRecElement = document.getElementById('dietFoodRec');
+    dietFoodRecElement.innerHTML = ''; // Clear previous recommendations
+
+    userDietData.forEach(item => {
+        const mealTypes = ['breakfast', 'lunch', 'snacks', 'dinner'];
+
+        mealTypes.forEach(mealType => {
+            const mealData = item.meals[mealType];
+
+            // Get an image URL based on the meal type (replace these with your actual image URLs)
+            const imageUrl = getImageUrlForMealType(mealType);
+
+            const cardElement = document.createElement('div');
+            cardElement.className = 'col';
+            cardElement.innerHTML = `
+            
+
+                <div class="card">
+                    <img src="${imageUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${mealType.charAt(0).toUpperCase() + mealType.slice(1)}</h5>
+                        ${mealData.map((food, index) => `
+                            <div class="">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">
+                                        <p><span class="fw-bold">${index + 1}.</span> ${food.food}</p>
+                                        <p><span class="fw-bold">Calories :</span> ${food.calories}</p>
+                                    </li>
+                                </ul>
+                                <hr>
+                            </div>
+                        `).join('')}
+                    </div>
+                </div>
+            `;
+
+            dietFoodRecElement.appendChild(cardElement);
+        });
+    });
+
+
+
+    // Function to get image URL based on meal type
+    function getImageUrlForMealType(mealType) {
+        // Add logic to return the image URL based on the meal type
+        // Replace these URLs with your actual image URLs
+        if (mealType === 'breakfast') {
+            return 'https://img.freepik.com/free-photo/english-breakfast-fried-eggs-sausages-zucchini-sweet-peppers_2829-19900.jpg?w=740&t=st=1703239168~exp=1703239768~hmac=b160fb156683a582252e297a99efa411a03aa928d94dccfe757bc0e9ce6098bd';
+        } else if (mealType === 'lunch') {
+            return 'https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141352.jpg?w=740&t=st=1703239242~exp=1703239842~hmac=9fd1eb115ccf13de65bb3d89501143c006b5c00b1f308f75e6ccced583f42754';
+        } else if (mealType === 'snacks') {
+            return 'https://img.freepik.com/free-photo/pretzels-chips-crackers-popcorn-bowls_114579-6490.jpg?w=740&t=st=1703239331~exp=1703239931~hmac=1eff8fdffc0d1caa4b41b4e1f353caebe7c0ba32d7c1e3a3e57c21402e61e373';
+        } else if (mealType === 'dinner') {
+            return 'https://img.freepik.com/free-photo/roasted-pork-steak-vegetables-plate_1150-45293.jpg?w=740&t=st=1703239530~exp=1703240130~hmac=1856fc9633b36c16dd59753fc9afc488f4da01c78d917222e6cc4ca94f2524c6';
+        } else {
+            // Default image URL if no match is found
+            return 'https://img.freepik.com/free-photo/front-view-cooked-rice-with-dough-slices-dark-surface-dish-meal-dark-food-photo_140725-81864.jpg?w=740&t=st=1703233990~exp=1703234590~hmac=62a5d60a4b260b9f6fd6456bb5b96e3ac8c6460b1785638ca99cd9e418db377d';
+        }
+    }
+
 }
